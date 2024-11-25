9ca85ec57e9c9bd5d8a9b309bc0c37b1a901c5ee
Joel Noyce <noyce.joel@gmail.com>
2024-11-25 21:27:38 +0000

    updated post request endpoint

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -2,14 +2,17 @@
 
 export async function getNews() {
   try {
-    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/api/news", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-store",
-      body: JSON.stringify({ paperUrl: "https://www.theguardian.com" }),
-    });
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/news`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+        body: JSON.stringify({ paperUrl: "https://www.theguardian.com" }),
+      }
+    );
 
     if (!response.ok) {
       const errorData = await response.json();
@@ -18,7 +21,6 @@ export async function getNews() {
 
     const data = await response.json();
     return data;
-
   } catch (error) {
     console.error("Error fetching news:", error);
     throw error instanceof Error ? error : new Error("Failed to fetch news");
