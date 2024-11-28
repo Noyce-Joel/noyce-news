d7cd562943b3116ad131c7c987ec95bc164ad38f
Joel Noyce <noyce.joel@gmail.com>
2024-11-28 00:37:31 +0000

    updated dependencies

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -36,7 +36,7 @@ export async function GET() {
           ],
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(
-        "https://github.com/Sparticuz/chromium/releases/download/v126.0.0/chromium-v126.0.0-pack.tar"
+        "https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar"
       ),
       headless: chromium.headless,
     });
