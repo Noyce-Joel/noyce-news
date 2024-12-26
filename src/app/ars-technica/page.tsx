1679957973d38362ebf73b5b3cb59ea8499a2e60
Joel Noyce <noyce.joel@gmail.com>
2024-12-26 17:22:37 +0000

    ars technica to vercel json

--- src/app/ars-technica/page.tsx
+++ src/app/ars-technica/page.tsx
@@ -3,10 +3,8 @@
 
 import { NewsLoadingStates } from "@/components/loading/news-loading-states";
 import NewsStories from "@/components/stories/news-stories";
-import { getArsTechnica } from "@/lib/actions";
 
 import { useArticles } from "@/state/news-provider";
-import { useEffect } from "react";
 
 export default function Home() {
   const { news } = useArticles();
